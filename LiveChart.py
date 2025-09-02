3be0e5bad762936a780fd78ae36f53bf032ece1c
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-02 16:54:39 +0300

    Add files via upload

---
+++ LiveChart.py
@@ -0,0 +1,10 @@
+import matplotlib.pypot as plt
+import numpy as np
+
+#Creating a sample data
+months = ['Jan', 'Feb', 'March', 'April']
+sales = [536, 8939, 1000, 67378]
+
+x = np.array([300, 100, 12, 562, 266])
+#Simple data for three products
+y = 
--- pillowz.py
+++ pillowz.py
@@ -5,3 +5,12 @@ import os
 #open an image file
 image = Image.open(mypass.jpg)
 print("Original Image Size:", image.size)
+
+resized_image = image.resize((400, 600))
+print("Resized Image Size:", resized_image)
+resized_image.save('Resized_mypass.jpg')
+#Display the image
+resized_image.show()
+
+# Flipping and rotating/ transposing/ flipping vertically
+#Enhancing the contrast
