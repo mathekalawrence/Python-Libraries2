18ca5eb3d66d2bb4c4eb36e752c06fe82e2121b6
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-09 13:14:56 +0300

    Add files via upload

---
+++ nampy2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a_array = np.arange(10, 51)
+print("My Array is:", a_array)
+
+print("The minimum value is:", np.min(a_array))
+print("The highest value is: ", np.max(a_array))
+
+print("The array * 3: ", a_array*3)
