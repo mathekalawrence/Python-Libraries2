07b68a9537daf9c8d3aba981de9462a58d645065
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-09 14:31:04 +0300

    Add files via upload

--- metplot1.py
+++ metplot1.py
@@ -10,3 +10,24 @@ plt.title("A Line Plot")
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.show()
+
+#Creating a Bar Chart
+# Sample data
+names = ['Alice', 'Bob', 'Charlie']
+scores = [85, 90, 78]
+
+plt.bar(names, scores, color='skyblue')
+plt.title("Student Scores")
+plt.xlabel("Students")
+plt.ylabel("Scores")
+plt.show()
+
+#A pe Chart
+# Sample data
+activities = ['Sleeping', 'Eating', 'Coding', 'Gaming']
+hours = [8, 2, 8, 6]
+
+plt.pie(hours, labels=activities, autopct='%1.1f%%')
+plt.title("Daily Activities")
+plt.show()
+print(pie)
