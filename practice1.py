fa9410b093ffa95ecfb483abcbc1c38f876efd5e
mathekalawrence <mathekalawrence96@gmail.com>
2025-09-09 12:28:35 +0300

    I had to come up again

--- practice1.py
+++ practice1.py
@@ -1,5 +1,16 @@
 import math
+import random
+import datetime
 
 print(math.sqrt(56))
 print(math.pi)
-print(math.acos(345))
+
+#print(math.acos(345))
+print("Power of 4^6:", math.pow(4, 6))
+print("Random number between 2 and 27:", random.randint(2, 27))
+
+today = datetime.date.today()
+print("The date is: ", today)
+
+now = datetime.datetime.now()
+print("The current time is:", now.strftime("%H:%M:%S"))
