14f0d89169e60335431abc60008e3d7a0ba464cb
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-09 13:52:50 +0300

    Add files via upload

--- pandaz2.py
+++ pandaz2.py
@@ -5,3 +5,16 @@ data ={
     'Age': [27, 25, 30, 61],
     'Score': [78, 89, 75, 97]
 }
+
+df = pd.DataFrame(data)
+print(df)
+
+#Accessing the columns
+print("Usernames:", df['Username'])
+
+#Filter rows
+print("Scores above 90:")
+print(df[df['Score']>90])
+
+df = pd.read_csv('students.csv')
+print(df.head()) #Shows first 5 rows
