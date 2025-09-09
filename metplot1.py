9b5fcc82180cb48cac3d0b204eb2df11a512d7fc
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-09 14:27:25 +0300

    Add files via upload

---
+++ metplot1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+#data Sample
+x= [34, 72, 10, 6, 30]
+y=[13, 5, 100, 43, 90]
+
+#Creating a line plot
+plt.plot(x,y)
+plt.title("A Line Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
