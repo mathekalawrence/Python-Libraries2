67f9cafbaedc2fb9011c935acc0fe03b2a6957a8
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-09 13:43:58 +0300

    Add files via upload

---
+++ pandaz2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+#Create a DataFrame(tabular structure)
+data ={
+    'Username': ['Muia', 'Larry', 'Kisaa', 'Matheka'],
+    'Age': [27, 25, 30, 61],
+    'Score': [78, 89, 75, 97]
+}
